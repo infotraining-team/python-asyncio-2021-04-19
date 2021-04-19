830217ed42cd81e20947e96dcfb7f8525b2e7521
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2021-04-19 14:31:18 +0200

    client

--- echo/echo_client.py
+++ echo/echo_client.py
@@ -1,17 +1,29 @@
 import asyncio
 import time
 
-async def echo_client(address):
-    #sock.connect(('localhost', 25000))
-    reader, writer = await asyncio.open_connection(*address)
+n = 0
 
+async def monitor():
+    global n
+    while True:
+        await asyncio.sleep(1)
+        print(f"{n} reqs/sec")
+        n = 0
+
+async def echo_client(address):
+    print("client is starting")
+    reader, writer = await asyncio.open_connection(*address)
+    global n
     while True:
         #sock.send(b'Hello from client')
         writer.write(b"Hello from client")
         await writer.drain()
         resp = await reader.read(1000)
-        print(b"got: " + resp)
-        time.sleep(1)
+        n += 1
 
 ## launch more than one client
-asyncio.run(echo_client(('', 25000)))
+## check connections per sec
+async def main():
+    await asyncio.gather(*[echo_client(('localhost', 25000)) for i in range(8)], monitor())
+
+asyncio.run(main())
